4f24b6afee80716f39a4a2cae559d066c06bf9ae
Marlon Lima <marlonlimax2012@gmail.com>
2023-01-27 15:49:09 -0300

    Creating the Base Screen and BottomNavigationBar

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/sign_up_screen.dart';
 import 'package:greengrocer/src/auth/config/custom_colors.dart';
+import 'package:greengrocer/src/base/base_screen.dart';
 import 'components/custom_text_field.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -109,7 +110,15 @@ class SignInScreen extends StatelessWidget {
                             borderRadius: BorderRadius.circular(18),
                           ),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (c) {
+                                return const BaseScreen();
+                              },
+                            ),
+                          );
+                        },
                         child: const Text(
                           'Entrar',
                           style: TextStyle(fontSize: 18),
---
+++ lib/src/base/base_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatelessWidget {
+  const BaseScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.red),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.green,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withAlpha(100),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Carrinho',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Pedidos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Home',
+          ),
+        ],
+      ),
+    );
+  }
+}

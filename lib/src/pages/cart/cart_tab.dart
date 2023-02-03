2aaa5d6507915132b9b9cdfaae58c5251726c488
Marlon Lima <marlonlimax2012@gmail.com>
2023-02-03 16:11:01 -0300

    Confirmation dialog - cart

--- lib/src/pages/cart/cart_tab.dart
+++ lib/src/pages/cart/cart_tab.dart
@@ -88,7 +88,11 @@ class _CartTabState extends State<CartTab> {
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18),
                         )),
-                    onPressed: () {},
+                    onPressed: () async {
+                      bool? result = await showOrderConfirmation();
+
+                      print(result);
+                    },
                     child: const Text(
                       'Concluir pedido',
                       style: TextStyle(
@@ -102,6 +106,45 @@ class _CartTabState extends State<CartTab> {
           )
         ],
       ),
+    );
+  }
+
+  Future<bool?> showOrderConfirmation() {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          title: const Text('Confirmação'),
+          content: const Text('Deseja realmente concluir o pedido?'),
+          actions: [
+            TextButton(
+              style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Não'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Sim'),
+            )
+          ],
+        );
+      },
     );
   }
 }

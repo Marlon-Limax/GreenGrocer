e6d4fe379cf17e85e5318a3c1519ab61cf133a18
Marlon Lima <marlonlimax2012@gmail.com>
2023-02-08 16:04:41 -0300

    Button tapping animation

--- lib/src/pages/home/components/category_tile.dart
+++ lib/src/pages/home/components/category_tile.dart
@@ -15,12 +15,13 @@ class CategoryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Align(
-        alignment: Alignment.center,
+    return Align(
+      alignment: Alignment.center,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(30),
+        onTap: onPressed,
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 6),
+          padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 3),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
             color: isSelected ? CustomColors.customSwatchColor : Colors.transparent,

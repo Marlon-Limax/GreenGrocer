d65c4b72e3037b474e4b3623a889209b00ba591a
Marlon Lima <marlonlimax2012@gmail.com>
2023-02-08 15:48:20 -0300

    Animation in PageView navigation

--- lib/src/pages/base/base_screen.dart
+++ lib/src/pages/base/base_screen.dart
@@ -34,7 +34,12 @@ class _BaseScreenState extends State<BaseScreen> {
         onTap: (index) {
           setState(() {
             currentIndex = index;
-            pageController.jumpToPage(index);
+            // pageController.jumpToPage(index);
+            pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 600),
+              curve: Curves.easeInOutQuart,
+            );
           });
         },
         type: BottomNavigationBarType.fixed,
